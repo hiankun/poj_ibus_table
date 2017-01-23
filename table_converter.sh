77675999efdaf543ccb6493e2b4d80a4560822a1
hiankun <hiankun@gmail.com>
2017-01-23 23:53:15 +0800

    update table_converter.sh and README.md

--- table_converter.sh
+++ table_converter.sh
@@ -8,7 +8,7 @@ BEGIN {OFS=""};
 {
     if (/^VALID_INPUT_CHARS/)
         print $1," ",$2," ",$3=gensub(/0123456789/,"",1,$3);
-    else if (/[[:alpha:]]*[^ptkh[:digit:]]\t.*\t.*/ && $3 > 10)
+    else if (/[[:alpha:]]*[^ptkh[:digit:]]\t.*\t.*/ && $3 >= 10)
         print $1=$1"a","\t",$2,"\t",$3;
     else if (/[[:alpha:]][2]\t.*\t.*/)
         print $1=gensub(/2$/,"s",1,$1),"\t",$2,"\t",$3;
